b21037f3283a07974c6fd54292919e06a1b9f297
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-13 10:06:23 -0300

    config database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
   return response.json({ mensage: 'world old on!!' });
