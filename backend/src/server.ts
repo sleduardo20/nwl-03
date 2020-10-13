746eed634d193678325c381b598596076de4b6b4
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-13 15:56:30 -0300

    validations fields

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import path from 'path';
 import './database/connection';
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
