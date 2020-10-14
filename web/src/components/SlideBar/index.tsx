172065939ced24918d5e91b272cf4aaf886a71e9
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-14 14:10:17 -0300

    created component SlideBar

---
+++ web/src/components/SlideBar/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import Buttom from 'components/Buttom';
+import mapMarkerImg from '../../assets/map-marker.svg';
+
+import './styles.css';
+
+const SlideBar: React.FC = () => {
+  return (
+    <aside>
+      <img src={mapMarkerImg} alt="Happy" />
+
+      <footer>
+        <Buttom icon={FiArrowLeft} color="#fff" to="/" />
+      </footer>
+    </aside>
+  );
+};
+
+export default SlideBar;
