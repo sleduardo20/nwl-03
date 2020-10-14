169fcb5461cc147ce59bd8fc5c6efec521227549
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-14 11:43:02 -0300

    add popup and marker at map

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import face from '../../assets/face.svg';
+import mapMarker from '../../assets/map-marker.svg';
 import Buttom from '../../components/Buttom';
 
 import { Container } from './styles';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarker,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -39,6 +49,20 @@ const OrphanagesMap: React.FC = () => {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        <Marker position={[-26.8750594, -49.0716105]} icon={mapIcon}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar dos canecos
+            <Link to="/">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Buttom to="/" icon={FiPlus} color="#fff" background="#15c3d6" />
--- web/src/pages/OrphanagesMap/styles.ts
+++ web/src/pages/OrphanagesMap/styles.ts
@@ -53,4 +53,35 @@ export const Container = styled.div`
   .leaflet-container {
     z-index: 5;
   }
+  .map-popup .leaflet-popup-content-wrapper {
+    background: rgb(255, 255, 255, 0.8);
+    border-radius: 20px;
+  }
+
+  .map-popup .leaflet-popup-content {
+    color: var(--blue);
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 8px 12px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .map-popup .leaflet-popup-content a {
+    width: 40px;
+    height: 40px;
+    background: var(--blue);
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+    border-radius: 12px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .map-popup .leaflet-popup-tip-container {
+    display: none;
+  }
 `;
